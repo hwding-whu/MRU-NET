23d9267b8c31efeb74b4206122fd8e18570850ce
hwding-whu <hwding32@163.com>
2023-01-06 10:45:06 +0800

    MRU-NET

---
+++ lib/help_functions.py
@@ -0,0 +1,158 @@
+import h5py
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from keras import backend as K
+
+def load_hdf5(infile):
+  with h5py.File(infile,"r") as f:  #"with" close the file after its nested commands
+    return f["image"][()]
+
+def write_hdf5(arr,outfile):
+  with h5py.File(outfile,"w") as f:
+    f.create_dataset("image", data=arr, dtype=arr.dtype)
+
+#convert RGB image in black and white
+def rgb2gray(rgb):
+    assert (len(rgb.shape)==4)  #4D arrays
+    assert (rgb.shape[1]==3)
+    bn_imgs = rgb[:,0,:,:]*0.299 + rgb[:,1,:,:]*0.587 + rgb[:,2,:,:]*0.114
+    bn_imgs = np.reshape(bn_imgs,(rgb.shape[0],1,rgb.shape[2],rgb.shape[3]))
+    return bn_imgs
+
+def green(rgb):
+    assert (len(rgb.shape)==4)  #4D arrays
+    assert (rgb.shape[1]==3)
+    bn_imgs = rgb[:,0,:,:]*0 + rgb[:,1,:,:]*1 + rgb[:,2,:,:]*0
+    bn_imgs = np.reshape(bn_imgs,(rgb.shape[0],1,rgb.shape[2],rgb.shape[3]))
+    return bn_imgs
+
+#group a set of images row per columns
+def group_images(data,per_row):
+    assert data.shape[0]%per_row==0
+    assert (data.shape[1]==1 or data.shape[1]==3)
+    data = np.transpose(data,(0,2,3,1))  #corect format for imshow
+    all_stripe = []
+    for i in range(int(data.shape[0]/per_row)):
+        stripe = data[i*per_row]
+        for k in range(i*per_row+1, i*per_row+per_row):
+            stripe = np.concatenate((stripe,data[k]),axis=1)
+        all_stripe.append(stripe)
+    totimg = all_stripe[0]
+    for i in range(1,len(all_stripe)):
+        totimg = np.concatenate((totimg,all_stripe[i]),axis=0)
+    return totimg
+
+
+#visualize image (as PIL image, NOT as matplotlib!)
+def visualize(data,filename):
+    assert (len(data.shape)==3) #height*width*channels
+    img = None
+    if data.shape[2]==1:  #in case it is black and white
+        data = np.reshape(data,(data.shape[0],data.shape[1]))
+    if np.max(data)>1:
+        img = Image.fromarray(data.astype(np.uint8))   #the image is already 0-255
+    else:
+        img = Image.fromarray((data*255).astype(np.uint8))  #the image is between 0-1
+    img.save(filename + '.png')
+    return img
+
+
+#prepare the mask in the right shape for the Unet
+def masks_Unet(masks):
+    assert (len(masks.shape)==4)  #4D arrays
+    assert (masks.shape[1]==1 )  #check the channel is 1
+    im_h = masks.shape[2]
+    im_w = masks.shape[3]
+    masks = np.reshape(masks,(masks.shape[0],im_h*im_w))
+    new_masks = np.empty((masks.shape[0],im_h*im_w,2))
+    for i in range(masks.shape[0]):
+        for j in range(im_h*im_w):
+            if  masks[i,j] == 0:
+                new_masks[i,j,0]=1
+                new_masks[i,j,1]=0
+            else:
+                new_masks[i,j,0]=0
+                new_masks[i,j,1]=1
+    return new_masks
+
+
+def pred_to_imgs(pred, patch_height, patch_width, mode="original"):
+    assert (len(pred.shape)==3)  #3D array: (Npatches,height*width,2)
+    assert (pred.shape[2]==2 )  #check the classes are 2
+    pred_images = np.empty((pred.shape[0],pred.shape[1]))  #(Npatches,height*width)
+    if mode=="original":
+        for i in range(pred.shape[0]):
+            for pix in range(pred.shape[1]):
+                pred_images[i,pix]=pred[i,pix,1]
+    elif mode=="threshold":
+        for i in range(pred.shape[0]):
+            for pix in range(pred.shape[1]):
+                if pred[i,pix,1]>=0.5:
+                    pred_images[i,pix]=1
+                else:
+                    pred_images[i,pix]=0
+    else:
+        print("mode " +str(mode) +" not recognized, it can be 'original' or 'threshold'")
+        exit()
+    pred_images = np.reshape(pred_images,(pred_images.shape[0],1, patch_height, patch_width))
+    return pred_images
+
+'''
+# parameter for loss function
+
+smooth = 1.
+
+#  metric function and loss function
+
+def dice_coef(y_true, y_pred):
+
+	y_true_f = K.flatten(y_true)
+
+	y_pred_f = K.flatten(y_pred)
+
+	intersection = K.sum(y_true_f * y_pred_f)
+
+	return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+ 
+def dice_coef_loss(y_true, y_pred):
+
+	return -dice_coef(y_true, y_pred)
+
+
+'''
+'''
+def dice_coef(y_true, y_pred, axis=(1, 2), smooth=1.):
+    intersection = tf.reduce_sum(y_true * y_pred, axis=axis)
+    union = tf.reduce_sum(y_true + y_pred, axis=axis)
+    numerator = tf.constant(2.) * intersection + smooth
+    denominator = union + smooth
+    coef = numerator / denominator
+
+    return tf.reduce_mean(coef)
+
+
+def dice_coef_loss(target, prediction, axis=(1, 2), smooth=1.):
+    """
+    """
+    intersection = tf.reduce_sum(prediction * target, axis=axis)
+    p = tf.reduce_sum(prediction, axis=axis)
+    t = tf.reduce_sum(target, axis=axis)
+    numerator = tf.reduce_mean(intersection + smooth)
+    denominator = tf.reduce_mean(t + p + smooth)
+    dice_loss = -tf.log(2.*numerator) + tf.log(denominator)
+
+    return dice_loss
+
+
+def combined_dice_ce_loss(y_true, y_pred, axis=(1, 2), smooth=1.,
+                          weight=args.weight_dice_loss):
+    """
+    Combined Dice and Binary Cross Entropy Loss
+    """
+    return weight*dice_coef_loss(y_true, y_pred, axis, smooth) + \
+        (1-weight)*K.losses.binary_crossentropy(y_true, y_pred)
+
+'''
